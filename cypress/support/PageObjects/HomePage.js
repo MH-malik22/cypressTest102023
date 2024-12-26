c9ddb9913cdddb3d208532b5c80cf63af1502268
MH-malik22 <mujamalik22@gmail.com>
2024-12-26 00:01:02 -0500

    added code example files

---
+++ cypress/support/PageObjects/HomePage.js
@@ -0,0 +1,27 @@
+class HomePage {
+   
+    static visitHomePage() {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        //cy.visit('https://demoqa.com/')
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export default HomePage;
--- cypress/support/step_definitions/HomepageSteps.js
+++ cypress/support/step_definitions/HomepageSteps.js
@@ -1,8 +1,10 @@
 import {Given, When, Then} from "cypress-cucumber-preprocessor/steps"
 import "../commands.js"
+import HomePage from "../../support/PageObjects/HomePage.js"
 
 
 Given('I am on the home page',()=>{
 
-    cy.visit('https://demoqa.com/')
+    HomePage.visitHomePage()
+
 })
